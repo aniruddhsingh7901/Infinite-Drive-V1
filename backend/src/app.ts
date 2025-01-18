fb8e3daa4cb9340f690eba0d3ed1728280c49408
Divya Soni <divya.soni@algocodingexperts.com>
2025-01-18 18:45:28 +0530

    first commit

---
+++ backend/src/app.ts
@@ -0,0 +1,80 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import path from 'path';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes';
+import { createServer } from 'http';
+import WebSocketService from './services/websocketService';
+import BlockCypherWebSocketService from './services/blockCypherService';
+
+// import orderRoutes from './routes/orderRoutes';
+import paymentRoutes from './routes/paymentRoutes';
+import bookRoutes from './routes/bookRoutes';
+import downloadRoutes from './routes/downloadRoutes';
+import webhookRoutes from './routes/blockCypher';
+import sequelize from './config/database';
+import './models'; // Import models to initialize them
+import bcrypt from 'bcrypt';
+import User from './models/userModel';
+import orderRoutes from './routes/orderRoutes';
+// Assuming you have a Token model
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+
+
+const server = createServer(app);
+const webSocketService = new WebSocketService(server);
+const blockCypherWebSocketService = new BlockCypherWebSocketService(webSocketService);
+
+app.use('/auth', authRoutes);
+app.use('/payment', paymentRoutes);
+app.use('/webhooks', webhookRoutes);
+app.use('/books', bookRoutes);
+app.use('/orders', orderRoutes)
+app.use('/download', downloadRoutes);
+
+
+
+const createAdminUser = async () => {
+  try {
+    const email = 'admin@example.com';
+    const password = 'adminpassword';
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const [user, created] = await User.findOrCreate({
+      where: { email },
+      defaults: { email, password: hashedPassword, role: 'admin' }
+    });
+
+    if (created) {
+      console.log('Admin user created successfully');
+    } else {
+      console.log('Admin user already exists');
+    }
+  } catch (error) {
+    console.error('Error creating admin user:', error);
+  }
+};
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.authenticate()
+  .then(async () => {
+    console.log('Database connected...');
+    await sequelize.sync(); 
+    await createAdminUser(); // Create admin user when the server starts
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err: any) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+export { webSocketService }; 
